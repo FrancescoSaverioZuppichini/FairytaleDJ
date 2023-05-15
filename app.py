4d2ab8e1d112a6be632793dd2fe2b566cc13752f
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2023-05-15 18:16:22 +0200

    new version with weighted probs when selecting song

--- app.py
+++ app.py
@@ -6,14 +6,27 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
 load_dotenv()
-import os
 import json
+import os
+import random
+from enum import Enum
+from typing import List, Tuple
+
+import numpy as np
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.schema import Document
 
 from data import load_db
 from names import DATASET_ID, MODEL_ID
-import random
+
+
+class RetrievalType:
+    FIRST_MATCH = "first-match"
+    POOL_MATCHES = "pool-matches"
+
+
+Matches = List[Tuple[Document, float]]
 
 
 @st.cache_resource
@@ -30,51 +43,125 @@ def init():
     )
 
     prompt = PromptTemplate(
-    input_variables=["songs", "user_input"],
-    template=Path("prompts/bot_with_summary.prompt").read_text(),
-)
+        input_variables=["user_input"],
+        template=Path("prompts/bot.prompt").read_text(),
+    )
 
-    llm = ChatOpenAI(temperature=0.7)
+    llm = ChatOpenAI(temperature=0.3)
 
     chain = LLMChain(llm=llm, prompt=prompt)
 
-    with open("data/emotions_with_spotify_url.json", "r") as f:
-        data = json.load(f)
-    
-    movies_and_names_to_songs = {}
+    return db, chain
 
-    songs_str = ""
 
-    for movie, songs in data.items():
-        for song in songs:
-            movie_and_name = f"{movie};{song['name']}".lower()
-            songs_str += f"{movie_and_name}:{song['text']}\n"
-            movies_and_names_to_songs[movie_and_name] = song
+# Don't show the setting sidebar
+if "sidebar_state" not in st.session_state:
+    st.session_state.sidebar_state = "collapsed"
 
-    return db, chain, movies_and_names_to_songs, songs_str
+st.set_page_config(initial_sidebar_state=st.session_state.sidebar_state)
 
-db, chain, movies_and_names_to_songs, songs_str = init()
 
-st.title("Disney song for you")
+db, chain = init()
+
+st.title("Disney songs for you 🎵🏰")
+st.markdown(
+    """
+*<small>Made with [DeepLake](https://www.deeplake.ai/) 🚀 and [LangChain](https://python.langchain.com/en/latest/index.html) 🦜⛓️</small>*
+
+💫 Unleash the magic within you with our enchanting app, turning your sentiments into a Disney soundtrack! 🌈 Just express your emotions, and embark on a whimsical journey as we tailor a Disney melody to match your mood. 👑💖""",
+    unsafe_allow_html=True,
+)
+how_it_works = st.expander(label="How it works")
 
 text_input = st.text_input(
     label="How are you feeling today?",
     placeholder="I am ready to rock and rool!",
 )
 
-clicked = st.button("Click me")
+run_btn = st.button("Make me sing! 🎶")
+with how_it_works:
+    st.markdown(
+        """
+The application follows a sequence of steps to deliver Disney songs matching the user's emotions:
+- **User Input**: The application starts by collecting user's emotional state through a text input.
+- **Emotion Encoding**: The user-provided emotions are then fed to a Language Model (LLM). The LLM interprets and encodes these emotions.
+- **Similarity Search**: These encoded emotions are utilized to perform a similarity search within our [vector database](https://www.deeplake.ai/). This database houses Disney songs, each represented as emotional embeddings.
+- **Song Selection**: From the pool of top matching songs, the application randomly selects one. The selection is weighted, giving preference to songs with higher similarity scores.
+- **Song Retrieval**: The selected song's embedded player is displayed on the webpage for the user. Additionally, the LLM interpreted emotional state associated with the chosen song is displayed.
+"""
+    )
+
+
 placeholder_emotions = st.empty()
 placeholder = st.empty()
 
-def get_emotions(songs_str, user_input):
-    res = chain.run(songs=songs_str, user_input=user_input)
-    song_key = random.choice(eval(res))
-    doc = movies_and_names_to_songs[song_key.lower()]
-    print(f"Reply: {res}, chosen: {song_key}")
-    with placeholder:
-        embed_url = doc["embed_url"]
-        iframe_html = f'<iframe src="{embed_url}" style="border:0"> </iframe>'
-        st.components.v1.html(f"<div style='display:flex;flex-direction:column'>{iframe_html}</div>")
 
-if clicked:
-    get_emotions(songs_str, text_input)
+with st.sidebar:
+    st.text("App settings")
+    filter_threshold = st.slider(
+        "Threadhol used to filter out low scoring songs",
+        min_value=0.0,
+        max_value=1.0,
+        value=0.8,
+    )
+    max_number_of_songs = st.slider(
+        "Max number of songs we will retrieve from the db",
+        min_value=5,
+        max_value=50,
+        value=20,
+        step=1,
+    )
+    number_of_displayed_songs = st.slider(
+        "Number of displayed songs", min_value=1, max_value=4, value=1, step=1
+    )
+
+
+def filter_scores(matches: Matches, th: float = 0.8) -> Matches:
+    return [(doc, score) for (doc, score) in matches if score > th]
+
+
+def normalize_scores_by_sum(matches: Matches) -> Matches:
+    scores = [score for _, score in matches]
+    tot = sum(scores)
+    return [(doc, (score / tot)) for doc, score in matches]
+
+
+def get_song(user_input: str, k: int = 20):
+    emotions = chain.run(user_input=user_input)
+    matches = db.similarity_search_with_score(emotions, distance_metric="cos", k=k)
+    # [print(doc.metadata['name'], score) for doc, score in matches]
+    docs, scores = zip(
+        *normalize_scores_by_sum(filter_scores(matches, filter_threshold))
+    )
+    choosen_docs = np.random.choice(docs, size=number_of_displayed_songs, p=scores)
+    return choosen_docs, emotions
+
+
+def set_song(user_input):
+    if user_input == "":
+        return
+    # take first 120 chars
+    user_input = user_input[:120]
+    docs, emotions = get_song(user_input, k=max_number_of_songs)
+    with placeholder_emotions:
+        st.markdown("Your emotions: `" + emotions + "`")
+    with placeholder:
+        iframes_html = ""
+        for doc in docs:
+            print(doc.metadata["name"])
+            embed_url = doc.metadata["embed_url"]
+            iframes_html += (
+                f'<iframe src="{embed_url}" style="border:0;height:100px"> </iframe>'
+            )
+
+        st.markdown(
+            f"<div style='display:flex;flex-direction:column'>{iframes_html}</div>",
+            unsafe_allow_html=True,
+        )
+        # st.components.v1.html(
+        #     f"<div>{iframes_html}</div>"
+        # )
+
+
+if run_btn:
+    set_song(text_input)
--- names.py
+++ names.py
@@ -1,4 +1,3 @@
 MODEL_ID = "text-embedding-ada-002"
 # DATASET_ID = "disney-lyrics"
 DATASET_ID = "disney-lyrics-emotions"
-
